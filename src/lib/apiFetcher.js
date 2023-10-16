105b6726fa4f7b74ec3dee25f04928b24e2038a4
Amran421 <78321589+Amran421@users.noreply.github.com>
2023-10-16 00:19:17 -0400

    v1.1.1 hotfix fixed athan sound not playing at athan time

--- src/lib/apiFetcher.js
+++ src/lib/apiFetcher.js
@@ -12,6 +12,8 @@ import {
     Qibla,
 } from 'adhan'
 
+let athanList = ['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'];
+
 export async function getAthanSounds() {
     let resourcePath = await resourceDir();
     let soundsPath = await join(resourcePath, '_up_', 'src', 'assets', 'sounds');
@@ -28,22 +30,6 @@ export async function getAthanSounds() {
     return athanNames;
 }
 
-// export async function getIp() {
-//     let ipGrabLink = 'https://api.ipify.org?format=json';
-
-//     let response
-
-//     try {
-//         response = await fetch(ipGrabLink, { method: 'GET', timeout: 30 });
-//     } catch (e) {
-//         console.log(e)
-//     }
-
-//     console.log(response?.data.ip)
-
-//     return response?.data.ip
-// }
-
 export function getTimes(coords) {
     console.log(coords)
     const coordinates = new Coordinates(coords.latitude, coords.longitude);
@@ -51,30 +37,10 @@ export function getTimes(coords) {
     const date = new Date();
     const prayerTimes = new PrayerTimes(coordinates, date, params);
 
+    for (let i = 0; i < athanList.length; i++) {
+        // console.log(prayerTimes[athanList[i]])
+        athanTime(prayerTimes[athanList[i]]);
+    }
+
     return prayerTimes;
 }
-
-// export async function getTimes() {
-//     let userIp = await getIp();
-//     let prayerTimesLink = `https://www.islamicfinder.us/index.php/api/prayer_times?user_ip=${userIp}`;
-
-//     let response
-//     try {
-//         response = await fetch(prayerTimesLink, { method: 'GET', timeout: 30 });
-//     } catch (e) {
-//         response = undefined
-//         console.log(e)
-//     }
-
-//     if (response === undefined) return undefined
-
-
-//     let results = response?.data.results
-
-//     for (const key in results) {
-//         athanTime(results[key])
-//     }
-//     console.log(results)
-
-//     return results
-// }
--- src/lib/athanHandler.js
+++ src/lib/athanHandler.js
@@ -7,7 +7,7 @@ import ShortAthan from '../assets/sounds/ShortAthan.mp3';
 
 import { loadData, saveData } from './persistentstore';
 
-let athanRefereshed = true;
+// let athanRefereshed = true;
 
 let athanAudio;
 let AthanMap = {
@@ -20,7 +20,6 @@ let AthanMap = {
 
 
 export function handleAthanSwitch(name) {
-    // if (name === selectedAthan) return;
     saveData("SelectedAthan", name)
 }
 
@@ -43,44 +42,19 @@ export async function playAthan(name) {
 }
 
 export function stopAthan() {
-    // athanAudio = new Audio(AthanMap[selectedAthan]);
     athanAudio.pause();
     athanAudio.currentTime = 0;
 }
 
 export async function athanTime(athanTime) {
-    let prayer = athanTime.split(" ")
-    let time = prayer[0]
-    let timeSuffix = prayer[1].replaceAll("%", "").trim()
-
-    console.log(timeSuffix)
-
-    let timeSplit = time.split(":")
-    let timeHours = timeSuffix === "am" ? Number(timeSplit[0]) : Number(timeSplit[0]) + 12
-    let timeSeconds = Number(timeSplit[1])
-
-
     let currentDate = new Date()
-    let prayerTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), timeHours, timeSeconds)
-    // console.log(prayerTime)
-
-    /* 
-     Create a global var named athanRefereshed
-     create timout set to go off at the start of next day
-     will update all athans and set refereshed var to true
-     check if true before setting timeout to prevent dupes 
-     [COMPLETED BUT NEEDS TESTING: +page.svelte:47]
-     */
+    // console.log(athanTime)
 
     let data = await loadData()
     let soundName = data.get("SelectedAthan")
 
-    if ((prayerTime.getTime() - currentDate.getTime()) < 0) return
+    if ((athanTime.getTime() - currentDate.getTime()) < 0) return
 
     // console.log(prayerTime.getTime() - currentDate.getTime())
-    setTimeout(playAthan, prayerTime.getTime() - currentDate.getTime(), soundName)
-
-    // if (!athanRefereshed) {
-    //     setTimeout()
-    // }
+    setTimeout(playAthan, athanTime.getTime() - currentDate.getTime(), soundName)
 }
